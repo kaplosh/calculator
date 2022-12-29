2cca5dcc80272cacd01f4ba1fbbbea22dde15b58
kaplosh <kaplanekt@gmail.com>
2022-12-29 13:53:57 +0100

    basic funcionalities, basic layout, needs to be debugged

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,70 @@ export default {
       curr_Value: "",
       first_Num: null,
       second_Num: null,
+      operation: null,
+      op_String: "",
+      result: null,
     };
   },
   methods: {
-    add_Num(added_Num) {
+    add_Number(added_Num) {
       this.curr_Value = String(this.curr_Value) + String(added_Num);
     },
 
-    addittion() {
-      console.log("addittion");
+    save_Value() {
       this.first_Num = this.curr_Value;
-      //this.curr_Value = "";
+      this.curr_Value = "";
+      console.log(this.curr_Value);
       console.log(this.first_Num);
+
+
     },
+    set_operation(param) {
+      this.operation = param;
+      if(this.operation === 1){
+        this.op_String = "+"
+      } else if (this.operation === 2) {
+        this.op_String = "-"
+      } else if (this.operation === 3){
+        this.op_String = "*"
+      } else if (this.operation === 4) {
+        this.op_String = "/"
+      }
+    },
+    make_result() {
+      this.second_Num = this.curr_Value;
+      if(this.operation === 1) {
+        this.result = Number(this.first_Num) + Number(this.second_Num);
+
+        this.curr_Value = "";
+      } else if(this.operation === 2 ) {
+        this.result = Number(this.first_Num) - Number(this.second_Num);
+        this.curr_Value = this.result;
+        this.curr_Value = "";
+      } else if (this.operation === 3 ) {
+        this.result = Number(this.first_Num) * Number(this.second_Num);
+        this.curr_Value = this.result;
+        this.curr_Value = "";
+      } else if (this.operation === 4) {
+        this.result = Number(this.first_Num) / Number(this.second_Num);
+        this.curr_Value = this.result;
+        this.curr_Value = "";
+      }
+
+    },
+    del_Single_Char(){
+      this.curr_Value = this.curr_Value.slice(0, -1);
+    },
+    clear_all(){
+      this.curr_Value = "";
+      this.first_Num = null;
+      this.second_Num = null;
+      this.operation = null;
+      this.result = null;
+      this.op_String = "";
+
+    }
+
   },
 };
 console.log("Hi");
@@ -28,48 +79,48 @@ console.log("Hi");
   <main>
     <div class="container">
       <div>
-        <button type="button" @click="add_Num(9)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(9)" class="btn btn-outline-light">
           9
         </button>
-        <button type="button" @click="add_Num(8)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(8)" class="btn btn-outline-light">
           8
         </button>
-        <button type="button" @click="add_Num(7)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(7)" class="btn btn-outline-light">
           7
         </button>
-        <button type="button" @click="add_Num(6)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(6)" class="btn btn-outline-light">
           6
         </button>
-        <button type="button" @click="add_Num(5)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(5)" class="btn btn-outline-light">
           5
         </button>
-        <button type="button" @click="add_Num(4)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(4)" class="btn btn-outline-light">
           4
         </button>
-        <button type="button" @click="add_Num(3)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(3)" class="btn btn-outline-light">
           3
         </button>
-        <button type="button" @click="add_Num(2)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(2)" class="btn btn-outline-light">
           2
         </button>
-        <button type="button" @click="add_Num(1)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(1)" class="btn btn-outline-light">
           1
         </button>
-        <button type="button" @click="add_Num(0)" class="btn btn-outline-light">
+        <button type="button" @click="add_Number(0)" class="btn btn-outline-light">
           0
         </button>
       </div>
       <div>
-        <button type="button" @click="addittion()" class="btn btn-outline-light">+</button>
-        <button type="button" class="btn btn-outline-light">-</button>
-        <button type="button" class="btn btn-outline-light">*</button>
-        <button type="button" class="btn btn-outline-light">/</button>
-        <button type="button" class="btn btn-outline-light">Del</button>
-        <button type="button" class="btn btn-outline-light">Clear</button>
-        <button type="button" class="btn btn-outline-light">=</button>
+        <button type="button" @click="save_Value(); set_operation(1)" class="btn btn-outline-light">+</button>
+        <button type="button" @click="save_Value(); set_operation(2)" class="btn btn-outline-light">-</button>
+        <button type="button" @click="save_Value(); set_operation(3)" class="btn btn-outline-light">*</button>
+        <button type="button" @click="save_Value(); set_operation(4)" class="btn btn-outline-light">/</button>
+        <button type="button" @click="del_Single_Char()" class="btn btn-outline-light">Del</button>
+        <button type="button" @click="clear_all()" class="btn btn-outline-light">Clear</button>
+        <button type="button" @click="make_result()" class="btn btn-outline-light">=</button>
       </div>
       <div>
-        {{ curr_Value }}
+        {{ curr_Value }} <br> {{ result }}
       </div>
     </div>
   </main>
